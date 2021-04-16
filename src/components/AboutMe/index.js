d089800c97c073bd28ca65706afadb71492c0783
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-04-16 14:46:49 -0400

    updating Portfolio

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -18,8 +18,10 @@ function AboutMe() {
 
           <p>
                 Hello! Welcome! My name is Josepine Hilliard. Feel free to call me Jodie.If my friends could describe me, they would say I’m resourceful. I
-                am very good at finding things, tangible and intangible.I am a hard-working, self-motivated and ambitious individual with proficiency in HTML, JavaScript, and CSS, 
-                as well as ability to communicate effectively in a team setting, and possessing a strong willingness to learn about 
+                am very good at finding things, tangible and intangible. Throughout my work history, I had the pleasure of using many different applications 
+                and software. I began to think about their inner workings. So I enrolled in a Rutgers coding boot camp. It taught me a lot about coding and
+                programming and I created a few functional applications. I am a hard-working, self-motivated and ambitious individual with proficiency in HTML, NodeJS, React,
+                JavaScript, MySQL, Bootstrap, and CSS, as well as ability to communicate effectively in a team setting, and possessing a strong willingness to learn about 
                 entrepreneurship and business seeks to apply for an entry level Full Stack Developer position.
           </p>
         </div>
---
+++ src/components/Works/index.js
@@ -0,0 +1,44 @@
+[
+    {
+        "id": 1,
+        "name": "Covid-Relief",
+        "image": "https://user-images.githubusercontent.com/77857718/114967654-d98f2800-9e42-11eb-94bd-df858bdc3742.png",
+        "github": "https://github.com/JakeMelville/covid19-relief",
+        "deploy": "https://covid-19-relief.herokuapp.com/"
+    },
+    {
+        "id": 2,
+        "name": "Google Books",
+        "image": "https://user-images.githubusercontent.com/77857718/114339416-22816c80-9b23-11eb-9de4-cc6d3b83d2d4.png",
+        "github": "https://github.com/dvaghani94/google-books",
+        "deploy": "https://google-books-dv.herokuapp.com"
+    },
+    {
+        "id": 3,
+        "name": "Fitness Tracker",
+        "image": "https://user-images.githubusercontent.com/77857718/112773892-cd633800-9005-11eb-9c55-e749872119b4.png",
+        "github": "https://github.com/dvaghani94/fitness-tracker",
+        "deploy": "https://fitness-tracker-dv.herokuapp.com/"
+    },
+    {
+        "id": 4,
+        "name": "Tech Blog",
+        "image": "https://user-images.githubusercontent.com/77857718/114967163-eeb78700-9e41-11eb-9509-ba8a849d2957.png",
+        "github": "https://github.com/dvaghani94/tech-blog",
+        "deploy": "https://tech-blog-dv.herokuapp.com"
+    },
+    {
+        "id": 5,
+        "name": "Avengers",
+        "image": "https://user-images.githubusercontent.com/77857718/114967301-34744f80-9e42-11eb-8d24-e6400b4a8028.png",
+        "github": "https://dvaghani94.github.io/avengers/",
+        "deploy": "https://github.com/JakeMelville/avengers"
+    },
+    {
+        "id": 6,
+        "name": "Note Taker",
+        "image": "https://user-images.githubusercontent.com/77857718/110229156-64404700-7ed5-11eb-80e9-403ac9f76e26.png",
+        "github": "https://github.com/dvaghani94/note-taker",
+        "deploy": "https://secret-fjord-87517.herokuapp.com"
+    }
+]
