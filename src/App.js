a779e9e64f5b8cfe8bd8439709b881e5efd535a5
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-04-16 13:53:20 -0400

    adding components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
